1c0981098019c0143410d052ab2476bbdbff78de
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-31 16:30:46 +0500

    Task 05

--- if05.py
+++ if05.py
@@ -9,16 +9,18 @@ def main(a,b,c):
         integer: the number of negative numbers in the given numbers
     """
     if a<0 and b<0 and c<0:
-        return 3
-    if a<0 and b<0:
+        return "3"
+    elif a<0 and b<0:
         return 2
-    if a<0 and c<0:
+    elif a<0 and c<0:
         return 2
-    if b<0 and c<0:
+    elif b<0 and c<0:
         return 2
-    if a<0:
+    elif a<0:
         return 1
     if b<0:
         return 1
     if c<0:
         return 1
+    else:
+        return 0
